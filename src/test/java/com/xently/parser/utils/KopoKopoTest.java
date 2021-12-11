82e38e891518552621f33d791ded79d859f2a67c
E-africa <efricashopping@gmail.com>
2021-12-11 17:26:58 +0300

    optimize files

--- src/test/java/com/xently/parser/utils/KopoKopoTest.java
+++ src/test/java/com/xently/parser/utils/KopoKopoTest.java
@@ -3,20 +3,24 @@ package com.xently.parser.utils;
 import junit.framework.TestCase;
 import org.junit.Assert;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class KopoKopoTest extends TestCase {
 
     public void test_case_1() {
-        Assert.assertEquals(Collections.singletonList("K20008"), KopoKopo.getBestCustomerIDs("/home/mitch/Projects/IntellijIDEA/shapes-parser/transaction_data_1.csv", 1));
+        Assert.assertEquals(Collections.singletonList("K20008"), KopoKopo.getBestCustomerIDs(System. getProperty("user.dir")+File.separator+"transaction_data_2.csv", 1));
     }
 
     public void test_case_2() {
-        Assert.assertEquals(Arrays.asList("K20987", "K20008", "K20233"), KopoKopo.getBestCustomerIDs("/home/mitch/Projects/IntellijIDEA/shapes-parser/transaction_data_2.csv", 3));
+        System.out.println(System. getProperty("user.dir")+File.separator+"transaction_data_2.csv");
+        Assert.assertEquals(Arrays.asList("K20987", "K20008", "K20233"), KopoKopo.getBestCustomerIDs(System. getProperty("user.dir")+File.separator+"transaction_data_2.csv", 3));
+
     }
 
     public void test_case_3() {
-        Assert.assertEquals(Arrays.asList("K20002", "K20005", "K20377", "K20987", "K22584"), KopoKopo.getBestCustomerIDs("/home/mitch/Projects/IntellijIDEA/shapes-parser/transaction_data_3.csv", 5));
+        Assert.assertEquals(Arrays.asList("K20002", "K20005", "K20377", "K20987", "K22584"), KopoKopo.getBestCustomerIDs(System. getProperty("user.dir")+File.separator+"transaction_data_3.csv", 5));
     }
+
 }
